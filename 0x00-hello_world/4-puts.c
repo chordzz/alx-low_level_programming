c8b45003ce1bf2eb73744acfa53f237542e3735a
Olaife Olawore <olaifeolawore@gmail.com>
2022-03-10 08:06:02 -0800

    modified failed tasks from 1-5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description: This is the main function of the app
+ * Return: zero
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description: This is the main function of the app
+ * Return: zero
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
