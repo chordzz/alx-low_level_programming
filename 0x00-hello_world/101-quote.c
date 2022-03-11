ba284cf7cca2a21d91e88c0a1feeca9370336c9f
Olaife Olawore <olaifeolawore@gmail.com>
2022-03-10 19:30:52 -0800

    advanced tasks begining

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description: This is the main function of the app
+ * Return: zero
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" = Dora Korpar, 2015-10-19\n") 2>;
+}
