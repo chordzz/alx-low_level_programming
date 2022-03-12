41755e5949e78f0eb3c1baa09c6221a1e52661b3
Olaife Olawore <olaifeolawore@gmail.com>
2022-03-11 20:57:01 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point of the app
+ *
+ * Description: Main function of the program
+ * Return: zero
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (!(letter = 'q') && !(letter = 'e'))
+		{
+			putchar(letter);
+		}
+
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
