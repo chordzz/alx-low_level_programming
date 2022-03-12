66233fde31564f75da7c2a3268650a22cad289b4
Olaife Olawore <olaifeolawore@gmail.com>
2022-03-11 21:57:59 -0800

    task 7 done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point of the app
+ *
+ * Description: Main function of the program
+ * Return: zero
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+	{
+		putchar(letter);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
