c7a1d5b97ac4ed97bd6036b224ad39def2bd3371
Olaife Olawore <olaifeolawore@gmail.com>
2022-03-16 04:42:31 -0700

    test commit 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,24 @@
-#include <stdio>
+#include "main.h"
 
 /*
  * main - entry point for the program
  *
- * Description -  
+ * Description: program to print _putchar
+ * Return: zero
+ */
+
+int main()
+{
+	char text[9] = "_putchar";
+
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+int _putchar(char c);
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to output
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, -1));
+}
