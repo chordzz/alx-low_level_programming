33f8f40ed52f0a87882dc78b9833378ef9aa8cb0
Olaife Olawore <olaifeolawore@gmail.com>
2022-03-22 12:52:51 -0700

    task 0 pointers, Arrays, Strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+ * reset_to_98 - resets the value of a parameter to 98
+ * @*n: a pointer to the address to be reset
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
