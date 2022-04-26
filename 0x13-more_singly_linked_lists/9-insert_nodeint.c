377042ace29eac055bd1bcad9cc33759bfcce49a
Olaife Olawore <olaifeolawore@gmail.com>
2022-04-26 07:05:42 -0700

    modified 9 0x13 -1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (current != NULL)
 	{
-		if (count == (index - 1))
+		if (count == (idx - 1))
 		{
 			new->next = current->next;
 			current->next = new;
