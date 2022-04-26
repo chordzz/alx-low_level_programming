57ec1eacc6b91c6ed045c5db779366364a361df5
Olaife Olawore <olaifeolawore@gmail.com>
2022-04-26 04:54:35 -0700

    test commit -3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,14 +13,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new;
 	listint_t *current;
 
-	void(current);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	current = *head;
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		*head = new;
 	}
@@ -32,5 +31,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		current->next = new;
 	}
-	return (*head);
+	return (new);
 }
