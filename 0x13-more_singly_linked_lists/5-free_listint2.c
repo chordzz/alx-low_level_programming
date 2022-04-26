53392fb10c57310bd4a7e0fefe0ce2ac7b08962a
Olaife Olawore <olaifeolawore@gmail.com>
2022-04-26 05:14:21 -0700

    4-5 0x13 modified

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current;
+	listint_t *temp;
 
 	if (head != NULL)
 	{
-		while ((current = head) != NULL)
+		current = *head;
+		while ((temp = current) != NULL)
 		{
-			head = head->next;
-			free(current);
+			current = current->next;
+			free(temp);
 		}
 		*head = NULL;
 	}
