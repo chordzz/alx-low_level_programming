e9bd13d741d3a77d293f79cb84d062e1839b8734
Olaife Olawore <olaifeolawore@gmail.com>
2022-07-01 07:09:32 -0700

    modified task 0 0x1A

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash_table;
 	unsigned long int i;
 
-	hash_array = malloc(sizeof(hash_node_t) * size);
+	hash_array = malloc(sizeof(hash_node_t *) * size);
 	new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (!(hash_array))
