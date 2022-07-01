02b60dc0092ceeb8f00ad653f69592930151f915
Olaife Olawore <olaifeolawore@gmail.com>
2022-07-01 08:31:26 -0700

    modified task 3 0x1A

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,11 +13,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	hash_node_t *elem;
 
+
 	if (!(ht))
 		return (0);
 
 	if (key == NULL || *key == '\0')
 		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!(new_node))
@@ -26,8 +29,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	new_node->next = NULL;
 
-
-	idx = key_index((unsigned char *)key, ht->size);
 	elem = ht->array[idx];
 
 	if (elem == NULL)
@@ -36,8 +37,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		new_node->next = elem;
+		if (strcmp(elem->key, key) == 0)
+		{
+			strcpy(ht->array[idx]->value, strdup(value));
+		}
+		else
+		{
+			new_node->next = elem;
+			ht->array[idx] = new_node;
+		}
 	}
-
 	return (1);
 }
