ecf1f17717cb88ecfdbec503849ff1e4b682c080
Olaife Olawore <olaifeolawore@gmail.com>
2022-07-01 08:06:07 -0700

    completed 3 0x1A

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,5 +10,34 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	unsigned long int count;
+	hash_node_t *new_node;
+	hash_node_t *elem;
+
+	if (!(ht))
+		return (0);
+
+	if (key == NULL || *key == '\0')
+		return (0);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!(new_node))
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+
+	idx = key_index((unsigned char *)key, ht->size);
+	elem = ht->array[idx];
+
+	if (elem == NULL)
+	{
+		elem = new_node;
+	}
+	else
+	{
+		new_node->next = elem;
+	}
+
+	return (1);
 }
